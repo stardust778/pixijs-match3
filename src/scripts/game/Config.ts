f1b1ae8a83db12dac0561932b3f65115f18203e5
lirihao03 <lirihao03@kuaishou.com>
2023-12-08 10:42:31 +0800

    feat: 增加terserPlugin

--- src/scripts/game/Config.ts
+++ src/scripts/game/Config.ts
@@ -25,9 +25,9 @@ export const Config = {
     },
     tilesColors: ['blue', 'green', 'orange', 'red', 'pink', 'yellow'],
     combinationRules: [[
-        {col: 1, row: 0}, {col: 2, row: 0},
+        {col: 1, row: 0}, {col: 2, row: 0}
     ], [
-        {col: 0, row: 1}, {col: 0, row: 2},
+        {col: 0, row: 1}, {col: 0, row: 2}
     ]]
 };
 
